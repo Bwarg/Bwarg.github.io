df28e7a433ec90bdd58697024caa1c1d701451ac
Kitkat <samuel32259@gmail.com>
2018-06-05 14:28:32 -0400

    Update for grif

---
+++ button.js
@@ -0,0 +1,14 @@
+function add(x,y)
+{
+    return(x+y);
+}
+
+function change(x,y)
+{
+    document.getElementById("test").innerHTML= add(x,y);
+}
+
+function letuser()
+{
+    alert("Hey you are dumb.");
+}
